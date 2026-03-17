56db90cef8e8f0a9754ee1a2ffcbe96ec06a2922
maixarcher <maixarcher@gmail.com>
2026-03-17 18:27:10 +0800

    first upload

---
+++ src/main/java/com/example/demo/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.example.demo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+      private String mssage;
+      T data;
+      
+      public static<T> ApiResponse<T> success(String message,T data){
+    	  return new ApiResponse<T>(message,data);
+
+      }
+       public static <T> ApiResponse<T> error(String message) {
+    	  return new ApiResponse<T>(message,null);    	  
+      }  
+}
